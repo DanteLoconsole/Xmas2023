d2ea3d84dc721390e9c59420c63ed04acd4f35dc
Dante Loconsole <120262205+DanteLoconsole@users.noreply.github.com>
2023-12-23 14:02:29 +0100

    Add files via upload

---
+++ code.js
@@ -0,0 +1,29 @@
+function checkPassword(event) {
+    if (event.key === "Enter") {
+        var passwordInput = document.getElementById("password");
+        var enteredPassword = passwordInput.value;
+
+        if (enteredPassword === "test") {
+            window.open("https://google.com", "_blank");
+            passwordInput.value = "";
+            
+        } else {
+            passwordInput.style.borderColor = "red";
+            passwordInput.style.animation = "shake 0.5s";
+
+            setTimeout(function () {
+                passwordInput.style.borderColor = "";
+                passwordInput.style.animation = "";
+                passwordInput.value = "";
+            }, 500);
+        }
+    }
+}
+
+function ja() {
+    window.location = "password.html";
+}
+
+function nee() {
+    window.location = "cat.jpeg";
+}
